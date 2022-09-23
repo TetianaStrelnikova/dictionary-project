18a4e3c53005d4b0c3970655335d678846479b69
TetianaStrelnikova <strelnikova0108@gmail.com>
2022-09-24 01:55:56 +0400

    add css

--- src/Photos.js
+++ src/Photos.js
@@ -3,7 +3,7 @@ export default function Photos(props){
     console.log(props.photos)
     if(props.photos){
     return(
-        <div className="row p-5 tjustify-content-center backgroundLight m-5">{props.photos.map(function(photo,index){return(<img className="m-3 p-0 photo image-fluide col-3" src={photo.src.landscape} alt="search" key={index}/>);})}</div>
+        <div className="row p-5 justify-content-center backgroundLight m-5">{props.photos.map(function(photo,index){return(<img className="m-3 p-0 photo image-fluide col-3" src={photo.src.landscape} alt="search" key={index}/>);})}</div>
     );}
-    else{return "null";}
+    else{return null;}
 }
